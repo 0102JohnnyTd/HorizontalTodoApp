3c25e0a95bf13d5a79d4127f050450b537b53cc6
0102johnny <rt2824.recruit.1022@gmail.com>
2023-11-07 20:10:45 +0900

    Initial commit

---
+++ HorizontalTodoApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HorizontalTodoApp
+//
+//  Created by Johnny Toda on 2023/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
