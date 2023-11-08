a0b0ef92ac1a405b693c9e00b8177d1dd4696fb2
0102johnny <rt2824.recruit.1022@gmail.com>
2023-11-08 20:16:52 +0900

    [refactor] Cellを円形に変更

--- HorizontalTodoApp/TodoCell.swift
+++ HorizontalTodoApp/TodoCell.swift
@@ -13,17 +13,24 @@ final class TodoCell: UICollectionViewCell {
     static let nib = UINib(nibName: String(describing: TodoCell.self), bundle: nil)
     static let identifier = String(describing: TodoCell.self)
 
-
+    // ⚠️この時点ではCellのサイズは決定していない為、awakeFromNib内でレイアウトの設定は避けるべき。
     override func awakeFromNib() {
         super.awakeFromNib()
         setUpBackgroundView()
+    }
+
+    // 💡呼び出し時点で制約が確定しており、その制約をもとにViewのレイアウトを実行する
+    override func layoutSubviews() {
+        // Viewを円形にする
+        self.layer.cornerRadius = self.frame.width * 0.5
+        // backgroundViewのViewからはみ出た部分などを非表示にする
+        self.layer.masksToBounds = true
     }
 
     ///  BackgroundViewを生成する
     private func setUpBackgroundView() {
         backgroundView = UIView(frame: super.frame)
         backgroundView?.backgroundColor = .red
-        backgroundView?.layer.cornerRadius = self.frame.width * 0.5
     }
 
     /// 自身のオブジェクトに値を代入する
