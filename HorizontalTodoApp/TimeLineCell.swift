5b75dbf42fed027fd77891cc52a407a875d690d6
0102johnny <rt2824.recruit.1022@gmail.com>
2023-11-10 11:08:45 +0900

    [add] TimeLineに表示させるCellのViewをXIBで作成

---
+++ HorizontalTodoApp/TimeLineCell.swift
@@ -0,0 +1,29 @@
+//
+//  TimeLineCell.swift
+//  HorizontalTodoApp
+//
+//  Created by Johnny Toda on 2023/11/10.
+//
+
+import UIKit
+
+final class TimeLineCell: UICollectionViewCell {
+    @IBOutlet private weak var descriptionLabel: UILabel!
+
+    static let nib = UINib(nibName: String(describing: TimeLineCell.self), bundle: nil)
+    static let identifier = String(describing: TimeLineCell.self)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpBackgroundView()
+    }
+
+    private func setUpBackgroundView() {
+        backgroundView = UIView(frame: super.frame)
+        backgroundView?.backgroundColor = .systemBackground
+    }
+
+    func  configure(todo: String) {
+        descriptionLabel.text = "『\(todo)を完了しました』"
+    }
+}
