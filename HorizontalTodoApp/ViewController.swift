2e2c2bad675d5c253bf37e7bcf305a57235f170f
0102johnny <rt2824.recruit.1022@gmail.com>
2023-11-18 13:06:46 +0900

    [clean] 不要なコードを削除

--- HorizontalTodoApp/ViewController.swift
+++ HorizontalTodoApp/ViewController.swift
@@ -124,14 +124,6 @@ extension ViewController {
             supplementaryView.configure(name: "Todo")
         }
 
-        // ⚠️XIBファイル以外の実装方法(こっちで実装してしまっていた...)
-//        let TodoSectionHeaderRegistration =
-//        UICollectionView.SupplementaryRegistration<TodoSectionHeaderView>(elementKind: ElementKind.sectionHeader) { (supplementaryView, string, indexPath) in
-//            print("stringの中身：", string)
-//            print("supplementaryViewの中身：", supplementaryView)
-//
-//            supplementaryView.configure(name: "Todo")
-//        }
 
         // TimeLineCellの登録
         let timeLineCellRegistration = UICollectionView.CellRegistration<TimeLineCell, Item>(cellNib: TimeLineCell.nib) { cell, _, item in
